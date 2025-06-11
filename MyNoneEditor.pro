c3aa5b53e07f2edb2e0efd821c331f8cc1825fb2
mert coskun <mertt.coskunm@bitbucket.org>
2025-06-11 09:55:28 +0300

    Initial project structure

---
+++ MyNoneEditor.pro
@@ -0,0 +1,8 @@
+QT       += core gui widgets
+
+TARGET = MyNodeEditor
+TEMPLATE = app
+
+SOURCES += src/main.cpp \
+        
+
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QLabel label("Hello, Qt!");
+    label.resize(200, 100);
+    label.show();
+
+    return app.exec();
+}
