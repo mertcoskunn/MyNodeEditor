43e8232d47ddd5f3761ac2bb26e2a777b50065f1
mert coskun <mertt.coskunm@bitbucket.org>
2025-06-24 09:05:46 +0300

    added connection logic to pin class

--- src/nodes/Pin.cpp
+++ src/nodes/Pin.cpp
@@ -1,17 +1,15 @@
 #include "Pin.h"
+#include <QDebug>
 
 
-Pin::Pin(QGraphicsItem* parent)
+Pin::Pin(int type, QGraphicsItem* parent)
     :QGraphicsItem(parent)
 {
-    pinPolygon << QPointF(width/2,0)
-                  << QPointF(-width/2, -height/2)
-                  << QPointF(-width/2, height/2);
-
+    drawTrianglePin();
+    pinType = type;
     pinColor = Qt::cyan;
     setAcceptHoverEvents(true); 
 }
-
 
 QRectF Pin::boundingRect() const
 {
@@ -24,3 +22,109 @@ void Pin::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
         painter->setPen(Qt::black);
         painter->drawPolygon(pinPolygon);
 }
+
+QPointF Pin::getTriangleCenter() const
+{
+    if (pinPolygon.size() != 3) return QPointF(0, 0);
+    return (pinPolygon[0] + pinPolygon[1] + pinPolygon[2]) / 3.0;
+}
+
+void Pin::hoverEnterEvent(QGraphicsSceneHoverEvent *) 
+{
+        pinColor = Qt::yellow;
+        update();
+}
+
+void Pin::hoverLeaveEvent(QGraphicsSceneHoverEvent *)
+{
+    pinColor = Qt::cyan;
+    update();
+}
+
+void Pin::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+    event->accept();
+
+    QPointF startPos = mapToScene(boundingRect().center());
+
+    tempLine = new QGraphicsLineItem(QLineF(startPos, startPos));
+    tempLine->setPen(QPen(Qt::black, 2));
+    scene()->addItem(tempLine);
+}
+
+void Pin::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
+{
+    event->accept();
+
+    if (!tempLine)
+        return; 
+
+    QList<QGraphicsItem*> itemsUnderCursor = scene()->items(event->scenePos());
+
+    for (QGraphicsItem* item : itemsUnderCursor) {
+        Pin* tempPin = qgraphicsitem_cast<Pin*>(item);
+        if(tempPin && (tempPin != this))
+        {
+            pinColor= Qt::green;
+            update();
+        }
+    }
+
+    QPointF startPos = tempLine->line().p1();
+    QPointF currentPos = event->scenePos();
+
+    tempLine->setLine(QLineF(startPos, currentPos));
+}
+
+void Pin::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
+{
+    event->accept();
+
+    if(!tempLine)
+        return;
+    
+    QList<QGraphicsItem*> itemsUnderCursor = scene()->items(event->scenePos());
+    bool temp = false;
+
+    for (QGraphicsItem* item : itemsUnderCursor) 
+    {
+        if (!(item->type() == Pin::Type))
+            continue;
+        
+        Pin* tempPin = qgraphicsitem_cast<Pin*>(item);
+        
+        if((tempPin == nullptr) || (tempPin == this))
+            continue;
+
+        if(tempPin->parentItem() == this->parentItem())
+            continue;
+
+        if(tempPin->getPinType() == pinType)
+            continue;
+
+        ConnectionLine* line = new ConnectionLine(this, tempPin);
+                    
+        setLine(line);
+        scene()->addItem(line); 
+        scene()->removeItem(tempLine);
+        delete tempLine;
+        tempLine = nullptr;
+        temp = true;
+        break;
+    }
+
+    if(!temp)
+    {
+        scene()->removeItem(tempLine);
+        delete tempLine;
+        tempLine = nullptr;
+    }
+}
+
+void Pin::drawTrianglePin()
+{
+    pinPolygon << QPointF(width/2,0)
+               << QPointF(-width/2, -height/2)
+               << QPointF(-width/2, height/2);
+}
+
--- src/nodes/Pin.h
+++ src/nodes/Pin.h
@@ -4,20 +4,49 @@
 #include <QPainter>
 #include <QPolygonF>
 #include <QColor>
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsSceneHoverEvent>
+#include <QGraphicsLineItem>
+#include "ConnectionLine.h"
 
 class Pin: public  QGraphicsItem
 {
 public:
-    Pin(QGraphicsItem* parent = nullptr);
+    enum { Type = UserType + 1 };
+
+    Pin(int type, QGraphicsItem* parent = nullptr);
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) override;
 
     float getWidth(){ return width; };
     float getHeight(){ return height; };
+    int getPinType(){ return pinType; };
+    ConnectionLine* getLine(){ return connectionLine; };
+    void setLine(ConnectionLine* line){ connectionLine = line; }; 
+    QPointF getTriangleCenter() const;
+
+    int type() const override { return Type; }; 
+
+protected:
+    void hoverEnterEvent(QGraphicsSceneHoverEvent *) override;
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent *) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
 
 private:
     QPolygonF pinPolygon;
     QColor pinColor;
     float width = 20.0;
-    float height = 20.0; 
+    float height = 20.0;
+
+    ConnectionLine* connectionLine = nullptr;
+    QGraphicsLineItem* tempLine = nullptr;
+
+    void drawTrianglePin();
+    void drawCirclePin(); 
+
+    int pinType = 0; //0 input, 1 output;
+
 };
